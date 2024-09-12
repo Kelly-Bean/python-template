09d2b59eb38740a2dce99a8094a59e3f8c845909
Kelly Bean <kelly@komply.ai>
2024-09-12 18:34:57 -0500

    Add cfg.py

--- src/python_template/cfg.py
+++ src/python_template/cfg.py
@@ -2,4 +2,9 @@ from pathlib import Path
 
 
 THIS_DIR = Path(__file__).resolve().parent
-DATA_DIR = THIS_DIR.parent.parent / "resources" / "data"
+RESOURCES_DIR = THIS_DIR.parent.parent / "resources"
+DATA_DIR = RESOURCES_DIR / "data"
+GITIGNORED_CACHE_DIR = RESOURCES_DIR / "cachedir"
+
+GCP_PROJECT_ID = "bskb-data-prod"
+GCP_BUCKET_ID = "bskb-data-prod"
